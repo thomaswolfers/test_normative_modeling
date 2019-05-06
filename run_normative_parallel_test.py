fc955b783b5666b1c2d4d022639853f8493932df
Thomas Wolfers <th.wolfers@gmail.com>
2019-05-06 12:30:34 +0200

    Add files via upload

---
+++ run_normative_parallel_test.py
@@ -0,0 +1,11 @@
+# import of necessecarry modules
+import nispat
+
+# run normative parallel - set up the scripts to call the test files-
+# setup
+
+
+# run normative modeling test script
+
+nispat.normative_parallel.execute_nm()
+
